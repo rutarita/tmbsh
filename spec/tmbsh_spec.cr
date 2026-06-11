07567112b8b34e550dc3e6444d22638b613de14b
rutarita <derat0make@gmail.com>
2026-06-11 17:13:39 +1000

    tidied up tests

--- spec/tmbsh_spec.cr
+++ spec/tmbsh_spec.cr
@@ -5,7 +5,6 @@ describe TMBSH do
     it "runs examples" do
       interpreter = TMBSH::Interpreter.new
       path = Path["../examples/static"]
-      failed_files = [] of {Path, Exception}
       Dir.children(path).each do |file|
         target = path / file
         puts "Executing #{target}..."
@@ -13,6 +12,5 @@ describe TMBSH do
         puts
         interpreter.reset
       end
-      # failed_files.empty?.should be_true
   end
 end
