c4ddfa6d9c900a2c5d42d669771c2497d93abeb5
rutarita <derat0make@gmail.com>
2026-06-11 16:52:49 +1000

    added tests

--- spec/tmbsh_spec.cr
+++ spec/tmbsh_spec.cr
@@ -1,9 +1,18 @@
 require "./spec_helper"
 
-describe Tmbsh do
+describe TMBSH do
   # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+    it "runs examples" do
+      interpreter = TMBSH::Interpreter.new
+      path = Path["../examples/static"]
+      failed_files = [] of {Path, Exception}
+      Dir.children(path).each do |file|
+        target = path / file
+        puts "Executing #{target}..."
+        interpreter.execute_file(target)
+        puts
+        interpreter.reset
+      end
+      # failed_files.empty?.should be_true
   end
 end
