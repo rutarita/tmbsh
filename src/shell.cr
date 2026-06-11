067ce4ea5db85bf6e864394ff1f57a5893574449
rutarita <derat0make@gmail.com>
2026-06-11 16:52:18 +1000

    shell is now proper

--- src/shell.cr
+++ src/shell.cr
@@ -1,17 +1,26 @@
 require "./readline.cr"
-
-class TMBSH
+module TMBSH
+class Shell
 
   HISTORY_FILE_NAME = ".tmbsh_history"
 
   def initialize
-    (Path.home / HISTORY_FILE_NAME).open do |fd|
-      line = fd.gets
-      ReadLine.add_history(line)
+    begin
+      ::File.open(Path.home / HISTORY_FILE_NAME) do |fd|
+        loop do
+          if line = fd.gets
+            ReadLine.add_history(line)
+          else
+            break
+          end
+        end
+      end
+    rescue
     end
   end
 
   def mainloop
 
   end
+end
 end
