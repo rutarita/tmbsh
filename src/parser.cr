368489805f0c4b4dcf455da3e2952dc8ee221956
rutarita <derat0make@gmail.com>
2026-09-01 21:37:11 +1000

    i forgot to fix the conflict

--- src/parser.cr
+++ src/parser.cr
@@ -86,23 +86,13 @@ module TMBSH
         name = token.raw_value
         next_token
         node = if var = VARIABLES_AS_LITERALS[name]?
-<<<<<<< HEAD
                  Interpreter::SingleValueNode.new(var)
                else
                  Interpreter::SingleValueNode.new(
                    Interpreter::VariableRef.new(name)
                  )
                end
-        if token.kind.path_separator? || token.kind.string?
-=======
-          Interpreter::SingleValueNode.new(var)
-        else
-        Interpreter::SingleValueNode.new(
-          Interpreter::VariableRef.new(name)
-        )
-        end
         if token.kind.path_separator? || token.kind.string? || token.kind.question? || token.kind.splat?
->>>>>>> 95b2492 (fixed string interpolation a bit)
           parse_string(node)
         else
           node
